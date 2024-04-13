3396e6389f33bb69ef96b32ea1880123ccc02f19
Yasinj05 <yasinj7658@gmail.com>
2024-04-14 00:16:18 +0330

    Added unit test for auth middleware

---
+++ tests/unit/middlware/auth.test.js
@@ -0,0 +1,23 @@
+import auth from "../../../middleware/auth.mjs";
+import { User } from "../../../models/user.mjs";
+import { jest } from "@jest/globals";
+import mongoose from "mongoose";
+
+describe("auth middleware", () => {
+  it("should populate req.user with the payload of a valid JWT", () => {
+    const user = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const token = new User(user).generateAuthToken();
+    const req = {
+      header: jest.fn().mockReturnValue(token),
+    };
+    const res = {};
+    const next = jest.fn();
+
+    auth(req, res, next);
+
+    expect(req.user).toMatchObject(user);
+  });
+});
